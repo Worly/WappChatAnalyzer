66b5d07cf39bc9ddad59a238321df5d65b67d46e
Valentino Vukelic <tino.vukelic@gmail.com>
2021-06-24 11:20:51 +0200

    Add unique constraint to StatisticCache Name and Date

--- WappChatAnalyzer/Domain/StatisticCache.cs
+++ WappChatAnalyzer/Domain/StatisticCache.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace WappChatAnalyzer.Domain
 {
+    [Index("StatisticName", "ForDate", IsUnique = true, Name = "IX_Name_Date")]
     public class StatisticCache
     {
         public int Id { get; set; }
---
+++ WappChatAnalyzer/Migrations/20210624091701_AddUniqueToStatisticCache.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WappChatAnalyzer.Migrations
+{
+    public partial class AddUniqueToStatisticCache : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "StatisticName",
+                table: "StatisticCaches",
+                type: "varchar(255)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Name_Date",
+                table: "StatisticCaches",
+                columns: new[] { "StatisticName", "ForDate" },
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Name_Date",
+                table: "StatisticCaches");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "StatisticName",
+                table: "StatisticCaches",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(255)",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
